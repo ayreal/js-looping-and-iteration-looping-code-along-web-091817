cbc4a604daf3903ab76fc51c9950c51f5391cd93
Ariel Herman <aeh.herman@gmail.com>
2017-10-30 16:37:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function printBadges(args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(`Welcome ${args[i]}! You are employee #${i + 1}.`);
+  }
+  return args;
+}
+
+function tailsNeverFails() {
+  let count = 0;
+  while (Math.random() >= 0.5) {
+    count++;
+  }
+
+  return `You got ${count} tails in a row!`;
+}
